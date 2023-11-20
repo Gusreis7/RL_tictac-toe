e19a1a6b5d5f932c6c1b0b539f95875c3b61a838
Gustavo <gustavo.reis2@discente.ufg.br>
2023-11-20 14:02:18 -0300

    FIM

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# Reinforcement Learning Aplicado ao Jogo da Velha
+# Reinforcement Learning Aplicado ao Jogo da Velha 
 
-Este repositório utiliza o algoritmo **Q-learning** para ensinar a jogar o Jogo da Velha. A implementação de cada componente necessário para o Q-learning está organizada em classes.
+<h2>Este repositório utiliza o algoritmo Q-learning para aprender jogar o Jogo da Velha .</h2>
 
+Com o objetivo de iniciar meus estudos aprendizado por reforço, desenvolvi este projeto da implementação de um agente Q-learning aplicado ao Jogo da Velha. Utilizei Python para criar um ambiente de aprendizado onde um agente aprendeu a jogar o Jogo da Velha contra diferentes estratégias, como adversários aleatórios e até mesmo contra si mesmo.
+
+Para o desenvolvimento do codigo estruturei classes para o modelo de jogo, ambiente e o agente Q-learning.O agente foi treinado  ajustando os parâmetros de aprendizado, como taxa de aprendizado e fator de desconto, para maximização dos resultados.
+
+Após o treinamento, avaliei o desempenho do agente em confrontos contra oponentes aleatórios e suas próprias iterações. Isso me proporcionou uma compreensão prática de como o Q-learning pode ser aplicado a um cenário simples como o Jogo da Velha e detalhes técnicos de sua implementação. 
+
+
+A implementação de cada componente necessário para o Q-learning está organizada nas seguintes classes
 - **game_model:** Nesta classe, encontra-se a implementação do modelo do jogo em si, permitindo a interação com o jogo e os métodos relativos ao tabuleiro do jogo.
 
 - **environment:** Nesta classe, o ambiente de aprendizado é definido. Aqui, o `q_agent` interage com o modelo de jogo e é atualizado através das iterações.
@@ -13,27 +21,64 @@ Para treinar o `q_agent`, utiliza-se o código em `run.py` que possibilita a con
 - IA[x]vsRandom
 - IA[o]vsRandom
 
+O treinamento começa jogando n partidas com IA[x]vsRandom em seguida, n partidas com IA[o]vsRandom e em seguida n partidas IA[x]vsIA[o], isso é feito para que o modelo primeiro explore os estados de x e o contra o agente aleatorio e desenvolvem estrategias, e após isso refinam essas estrategias jogando contra si mesmos 
+
+Exemplo de treinamento de um modelo usando `run.py`:
+
+    ```
+    python run.py --rounds 7000 --epsilon 0.6 --alpha 0.4 --discount_factor 0.9
+    ```
+
+
 Após o treinamento do modelo, ele é salvo em um arquivo JSON com os valores da q_table, cujo nome especifica as configurações do experimento.
 E após cada treinamento, o modelo passa por uma etapa de teste de 1000 jogos em cada modelidade
 
-Para jogar contra o modelo, basta usar o código `play.py` e indicar onde está o arquivo de configuração do `q_agent` com a flag `--file_name`.
+Para jogar contra o modelo, basta usar o código `gradio_game.py` e indicar onde está o arquivo de configuração do `q_agent` com a flag `--file_name`.
 
 ## Analise 
-Para analise de resultados avaliei os resultados obtidos com o treinamento do q agent com 10,100,1000,10000,100000 jogos ,e com os seguintes parametros: alpha 0.6, epsilon = 0.5, discount_factor = 0.7
-Avaliando os resultados no treinameno 
+
+<h3>Treino</h3>
+Para  treinamento foram testados o q agent o seguintes parametros de aprendizado: com 10,100,1000,10000,100000 jogos ,e com os seguintes parametros: alpha 0.6, epsilon = 0.5, discount_factor = 0.7. Avaliando a evolução do treino conforme o numero de partidas:
 
 ![Alt text](image.png)
+
 Com os resultados acima do agente jogando contra si mesmo, percebe-se, um numero maior de partidas jogadas não melhorou os resultados, com o agente(x) ganhando um pouco mais que o agente(o) e resultando em poucos empates
+
 
 ![Alt text](image-1.png)
 
-Ao avaliarmos o agent(x) contra o agente aleatorio, percebe-se que ele passa por uma piora de 10 a 100 partidas jogadas, mas volta a melhorar com o crescimento das partidas jogadas, conseguindo ganhar por volta de 80% das vezes do agente aleatorio(o) e tendo poucos empates 
+Ao avaliarmos o agent(x) contra o agente aleatorio(o), percebe-se que ele passa por uma piora de 10 a 100 partidas jogadas, mas volta a melhorar com o crescimento das partidas jogadas, conseguindo ganhar por volta de 80% das vezes do agente aleatorio(o) e tendo poucos empates 
+
 
 ![Alt text](image-2.png)
-Este código é **Open Source**, sinta-se livre para usar, modificar e distribuir.
 
 Avaliando os resultados do agente(o) contra o agente(x) aleatorio percebe-se que conforme o numero de partidas cresce, ele consegue equilibrar mais o jogo ganhondo mais vezes do agente(x) aleatorio e tendo mais empates
 
+<h3>Teste</h3>
 
-Analise teste 
+Para analise dos resultados em teste o modelo foi testado em 1000 partidas, com epsilon = 0 isso implica que ele não fará explorações e portanto ira jogar com seu melhor movimento para cada estado.
 
+IA[x] vs IA[O]
+
+![Alt text](image-3.png)
+
+Avaliandos os resultados desse teste, o agente(o) perde mais vezes com um pouco numero de jogos para treino, conforme as rodadas de treino aumentam ele começa ficar melhor e começa a ganhar mais vezes, além disso o numero de empates também aumenta
+
+IA[x] vs random[O]
+
+O agente(x) mesmo com poucas rodadas de treino ja ganha muitas partidas contra o agente aleatorio o, ganhando 70% das vezes com apenas 10 rodadas de treino, e conforme o numero de rodadas aumenta ele fica muito bom ganhando até 97,7% das partidas 
+
+![Alt text](image-4.png)
+
+
+IA[O] vs random[X]
+
+![Alt text](image-5.png)
+
+O agent o começa equilibrado com o agente(x) aleatorio e conforme o numero de partidas no treino aumenta ele fica melhor ganhando mais vezes e empatando mais também, porém não consegue atingir o mesmo nivel de resultado do agente(x)
+
+
+Avaliando os resultados como um todo, fica claro que o agente(x), tem uma vantagem por começar primeiro, mas conforme os agentes aprendem, o agente(o) começa conseguir empatar ou ganhar mais vezes
+
+
+<h3>Este código é Open Source, sinta-se livre para usar, modificar e distribuir.</h3>
--- gradio_game.py
+++ gradio_game.py
@@ -2,6 +2,7 @@
 import gradio as gr
 import numpy as np
 import time
+import argparse
 from classes.game_model import tic_tac_toe_model
 from classes.Qlearningagent import QlearningAgent
 
@@ -24,165 +25,170 @@ custom_css = """
 
 usr = -2
 ia = -1
-
 game_over = False
 temp = -2
 
-  
-def usr_move(position,value):
-    global game_over
-    if value == '' and game_over == False:
-      usr_piece = 'X' if usr == 1 else 'O'
-      i,j = np.unravel_index(int(position)-1, shape=(3,3))
-      board.move(i,j,usr) 
-      board.print_game()
-      w = board.check_win()
+def main():
 
-      global temp
-      temp-=1
-      return gr.update(size='lg', scale = 0, min_width = 100, value=usr_piece, interactive=True), gr.Number(value=temp, visible=False), gr.Number(value=w, visible=False)
-    else:
-      return gr.update(size='lg', scale = 0, min_width = 100, interactive=True), gr.update(value=temp, visible=False),gr.update(visible=False)
-   
-       
-def ia_move():
-    global game_over
-    if game_over == False:
-      state_board = board.matriz.copy()
-      avaible_moves = board.get_avaible_moves()
-      action = q_agent.choose_move(state_board,avaible_moves,ia)
-      i, j = board.number_ij(action)
-      ia_piece = 'X' if ia == 1 else 'O'
-      board.move(i,j,ia)
-      board.print_game()
-      w = board.check_win()
-      #buttons
-      retornos_btn = [gr.Button(size='lg', scale = 0, min_width = 100, interactive=True)]*(action+1)
-      retornos_btn[-1] = gr.Button(size='lg', scale = 0, min_width = 100, interactive=True, value=ia_piece)
-      retornos_btn.extend([gr.Button(size='lg', scale = 0, min_width = 100,  interactive=True)]*(9 - action-1))
-      retornos_btn.append(gr.Number(value=w, visible=False))
-      return retornos_btn
-    else:
-      retornos_btn = [gr.Button(size='lg', scale = 0, min_width = 100, interactive=True)]*9
-      retornos_btn.append(gr.Number(visible=False))
-      return retornos_btn
-       
-
-def set_col_visible(selected):
-    global usr
-    global game_over
-    game_over = False
-    usr =  1 if selected  == 'X' else 2
-    global ia
-    ia = 2 if usr == 1 else 1
-    retorn_buttons = [gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=True)]*9
-    if selected == 'X':
-      retorn_buttons.append(gr.Number(value=temp, visible=False))
-
-    else:
-      retorn_buttons = ia_move()
-
-    return retorn_buttons
-  
-      
-def reset():
-  board.reset_matrix()
-  buttons = [gr.Button(size='lg', scale = 0, min_width = 100,elem_classes='gradio-button',interactive=False)]*9
-  return buttons
-
-
-def check(n):
-  msg = ''
-  global game_over
-  if n == 1:
-    game_over = True
-    msg = 'O jogador X ganhou'
-    board.reset_matrix()
-
-  elif n == 2:
-    game_over = True
-    msg = 'O jogador O ganhou'
-    board.reset_matrix()
-
-  elif n == 3 :
-    game_over = True
-    msg = 'VELHA'
-    board.reset_matrix()
- 
-  print('check',game_over)
-  return gr.update(value=msg, show_label=False)
-
-with gr.Blocks(css=custom_css, theme=gr.themes.Soft()) as demo:
-    q_agent= QlearningAgent(epsilon=0,alpha=0,discount_factor=0, train=False)
-    q_agent.load_agent_dict('models/q_agent-0.5ep-0.6ap-0.9-1000000r.json')
-    board = tic_tac_toe_model(3)
-    with gr.Row():
-      with gr.Column() as x:
-        title = gr.Text(value='Escolha uma peça', show_label=False)
-        with gr.Row():
-            p1 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-            p2 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-            p3 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-        with gr.Row():
-            p4 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-            p5 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-            p6 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-        with gr.Row():
-            p7 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-            p8 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-            p9 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
-
-      n1 = gr.Number(value="1", visible=False)
-      n2 = gr.Number(value="2", visible=False)
-      n3 = gr.Number(value="3", visible=False)
-      n4 = gr.Number(value="4", visible=False)
-      n5 = gr.Number(value="5", visible=False)
-      n6 = gr.Number(value="6", visible=False)
-      n7 = gr.Number(value="7", visible=False)
-      n8 = gr.Number(value="8", visible=False)
-      n9 = gr.Number(value="9", visible=False)
-      temp_number = gr.Number(value="10", visible=False)
-      win_number = gr.Number(value="4", visible=False)
-      game_state = gr.Number(value="0", visible=False)
-
-      with gr.Column():
-        c1 = gr.Radio( ['X','O'],label='Jogar como')
-        clear_button = gr.Button(value='Resetar')
-
-
-    buttons = [p1, p2, p3, p4, p5, p6, p7, p8, p9,win_number]
-    renders_components = [p1, p2, p3, p4, p5, p6, p7, p8, p9, win_number] 
-    game_components = [p1, p2, p3, p4, p5, p6, p7, p8, p9, title,temp_number,win_number, c1]
-    only_buttons = [p1, p2, p3, p4, p5, p6, p7, p8, p9]
-    p1.click(fn=usr_move,inputs=[n1,p1], outputs = [p1,temp_number,win_number])
-    p2.click(fn=usr_move,inputs=[n2,p2], outputs = [p2,temp_number,win_number])
-    p3.click(fn=usr_move,inputs=[n3,p3], outputs = [p3,temp_number,win_number])
-    p4.click(fn=usr_move,inputs=[n4,p4], outputs = [p4,temp_number,win_number])
-    p5.click(fn=usr_move,inputs=[n5,p5], outputs = [p5,temp_number,win_number])
-    p6.click(fn=usr_move,inputs=[n6,p6], outputs = [p6,temp_number,win_number])
-    p7.click(fn=usr_move,inputs=[n7,p7], outputs = [p7,temp_number,win_number])
-    p8.click(fn=usr_move,inputs=[n8,p8], outputs = [p8,temp_number,win_number])
-    p9.click(fn=usr_move,inputs=[n9,p9], outputs = [p9,temp_number,win_number])
-
-
-    def update_buttons():
+  def usr_move(position,value):
       global game_over
-      game_over = True
-      return {p1: '', p2:'', p3:'', p4:'', p5:'', p6:'', p7:'', p8:'', p9:'', 
-              title:'Escolha uma peça',temp_number:10, win_number:4, c1: ''}
-    
-   
+      if value == '' and game_over == False:
+        usr_piece = 'X' if usr == 1 else 'O'
+        i,j = np.unravel_index(int(position)-1, shape=(3,3))
+        board.move(i,j,usr) 
+        board.print_game()
+        w = board.check_win()
+
+        global temp
+        temp-=1
+        return gr.update(size='lg', scale = 0, min_width = 100, value=usr_piece, interactive=True), gr.Number(value=temp, visible=False), gr.Number(value=w, visible=False)
+      else:
+        return gr.update(size='lg', scale = 0, min_width = 100, interactive=True), gr.update(value=temp, visible=False),gr.update(visible=False)
+      
           
-    c1.select(fn=set_col_visible, inputs= c1, outputs=buttons)
+  def ia_move():
+        global game_over
+        if game_over == False:
+          state_board = board.matriz.copy()
+          avaible_moves = board.get_avaible_moves()
+          action = q_agent.choose_move(state_board,avaible_moves,ia)
+          i, j = board.number_ij(action)
+          ia_piece = 'X' if ia == 1 else 'O'
+          board.move(i,j,ia)
+          board.print_game()
+          w = board.check_win()
+          #buttons
+          retornos_btn = [gr.Button(size='lg', scale = 0, min_width = 100, interactive=True)]*(action+1)
+          retornos_btn[-1] = gr.Button(size='lg', scale = 0, min_width = 100, interactive=True, value=ia_piece)
+          retornos_btn.extend([gr.Button(size='lg', scale = 0, min_width = 100,  interactive=True)]*(9 - action-1))
+          retornos_btn.append(gr.Number(value=w, visible=False))
+          return retornos_btn
+        else:
+          retornos_btn = [gr.Button(size='lg', scale = 0, min_width = 100, interactive=True)]*9
+          retornos_btn.append(gr.Number(visible=False))
+          return retornos_btn
+          
+
+  def set_col_visible(selected):
+        global usr
+        global game_over
+        game_over = False
+        usr =  1 if selected  == 'X' else 2
+        global ia
+        ia = 2 if usr == 1 else 1
+        retorn_buttons = [gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=True)]*9
+        if selected == 'X':
+          retorn_buttons.append(gr.Number(value=temp, visible=False))
+
+        else:
+          retorn_buttons = ia_move()
+
+        return retorn_buttons
+      
+          
+  def reset():
+      board.reset_matrix()
+      buttons = [gr.Button(size='lg', scale = 0, min_width = 100,elem_classes='gradio-button',interactive=False)]*9
+      return buttons
 
 
-    temp_number.change(fn=ia_move,outputs=renders_components)
-    win_number.change(fn=check, inputs=win_number, outputs=title)
-    clear_button.click(fn=update_buttons,outputs=game_components)
-    clear_button.click(fn=reset,outputs=only_buttons)
+  def check(n):
+      msg = ''
+      global game_over
+      if n == 1:
+        game_over = True
+        msg = 'O jogador X ganhou'
+        board.reset_matrix()
+
+      elif n == 2:
+        game_over = True
+        msg = 'O jogador O ganhou'
+        board.reset_matrix()
+
+      elif n == 3 :
+        game_over = True
+        msg = 'VELHA'
+        board.reset_matrix()
+    
+      print('check',game_over)
+      return gr.update(value=msg, show_label=False)
+  
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--file_name', type=str, required=False, default='models/q_agent-0.9ep-0.6ap-0.9-1000r.json')
+  args = parser.parse_args()
+  with gr.Blocks(css=custom_css, theme=gr.themes.Soft()) as demo:
+      q_agent= QlearningAgent(epsilon=0,alpha=0,discount_factor=0, train=False)
+      q_agent.load_agent_dict(args.file_name)
+      board = tic_tac_toe_model(3)
+      with gr.Row():
+        with gr.Column() as x:
+          title = gr.Text(value='Escolha uma peça', show_label=False)
+          with gr.Row():
+              p1 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+              p2 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+              p3 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+          with gr.Row():
+              p4 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+              p5 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+              p6 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+          with gr.Row():
+              p7 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+              p8 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+              p9 = gr.Button(size='lg', scale = 0, min_width = 100,value='',elem_classes='gradio-button',interactive=False)
+
+        n1 = gr.Number(value="1", visible=False)
+        n2 = gr.Number(value="2", visible=False)
+        n3 = gr.Number(value="3", visible=False)
+        n4 = gr.Number(value="4", visible=False)
+        n5 = gr.Number(value="5", visible=False)
+        n6 = gr.Number(value="6", visible=False)
+        n7 = gr.Number(value="7", visible=False)
+        n8 = gr.Number(value="8", visible=False)
+        n9 = gr.Number(value="9", visible=False)
+        temp_number = gr.Number(value="10", visible=False)
+        win_number = gr.Number(value="4", visible=False)
+        game_state = gr.Number(value="0", visible=False)
+
+        with gr.Column():
+          c1 = gr.Radio( ['X','O'],label='Jogar como')
+          clear_button = gr.Button(value='Resetar')
+
+
+      buttons = [p1, p2, p3, p4, p5, p6, p7, p8, p9,win_number]
+      renders_components = [p1, p2, p3, p4, p5, p6, p7, p8, p9, win_number] 
+      game_components = [p1, p2, p3, p4, p5, p6, p7, p8, p9, title,temp_number,win_number, c1]
+      only_buttons = [p1, p2, p3, p4, p5, p6, p7, p8, p9]
+      p1.click(fn=usr_move,inputs=[n1,p1], outputs = [p1,temp_number,win_number])
+      p2.click(fn=usr_move,inputs=[n2,p2], outputs = [p2,temp_number,win_number])
+      p3.click(fn=usr_move,inputs=[n3,p3], outputs = [p3,temp_number,win_number])
+      p4.click(fn=usr_move,inputs=[n4,p4], outputs = [p4,temp_number,win_number])
+      p5.click(fn=usr_move,inputs=[n5,p5], outputs = [p5,temp_number,win_number])
+      p6.click(fn=usr_move,inputs=[n6,p6], outputs = [p6,temp_number,win_number])
+      p7.click(fn=usr_move,inputs=[n7,p7], outputs = [p7,temp_number,win_number])
+      p8.click(fn=usr_move,inputs=[n8,p8], outputs = [p8,temp_number,win_number])
+      p9.click(fn=usr_move,inputs=[n9,p9], outputs = [p9,temp_number,win_number])
+
+
+      def update_buttons():
+        global game_over
+        game_over = True
+        return {p1: '', p2:'', p3:'', p4:'', p5:'', p6:'', p7:'', p8:'', p9:'', 
+                title:'Escolha uma peça',temp_number:10, win_number:4, c1: ''}
+      
+    
+            
+      c1.select(fn=set_col_visible, inputs= c1, outputs=buttons)
+
+
+      temp_number.change(fn=ia_move,outputs=renders_components)
+      win_number.change(fn=check, inputs=win_number, outputs=title)
+      clear_button.click(fn=update_buttons,outputs=game_components)
+      clear_button.click(fn=reset,outputs=only_buttons)
 
 
 
-demo.launch()
+  demo.launch()
 
+if __name__ == '__main__':
+    main()
 
